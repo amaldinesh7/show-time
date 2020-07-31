6bcdd5bbc0c6705383d31d2111fa88c60d46dc05
amaldinesh4549 <amalkdinesh@gmail.com>
2020-07-31 15:32:19 +0530

    Error with Bell auth implemenataion

--- showtime-backend/server.js
+++ showtime-backend/server.js
@@ -1,20 +1,57 @@
 "use strict";
 
 const Hapi = require("hapi");
+const Bell = require("bell");
+// const Vision = require("vision"); start
 
 const { configureRoutes } = require("./src/routes/routes");
 
-const server = new Hapi.Server();
-server.connection({
-  port: 3001, host: "localhost", routes: {
-    cors: true
-  }
-});
+const init = async () => {
+  const server = new Hapi.Server({
+    connections: {
+      routes: {
+        cors: true,
+        payload: {
+          maxBytes: 16384
+        },
+        security: {
+          hsts: {
+            maxAge: 15552000,
+            includeSubdomains: true
+          },
+          xframe: true,
+          xss: true,
+          noOpen: false,
+          noSniff: true
+        },
+      }
+    }
+  });
+  server.connection({
 
-configureRoutes(server);
-server.start((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Server running at: ${server.info.uri}`);
-});
+    port: 3001, host: "localhost"
+  });
+
+  await server.register(Bell)
+  
+  server.auth.strategy('github', 'bell', {
+    provider: 'github',
+    password: 'cookie_encryption_password_secure',
+    isSecure: false,                                    // For testing or in environments secured via other means
+    clientId: '646cd12f2839d407d20b',
+    clientSecret: 'ea5ea3a82ecaf4862c69a08d0efdddf4c796a017',
+    location: 'http://localhost:3001',
+    scope: []
+  });
+
+
+  configureRoutes(server);
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+  });
+}
+
+init();
